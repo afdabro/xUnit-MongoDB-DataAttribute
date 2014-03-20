36779a5a718905c1d25c72cda2bbac83cf9efd99
afdabro <andrewfdabrowski@gmail.com>
2014-03-19 22:23:40 -0400

    Implemented MongoDB Repository for xUnit Data Source

---
+++ xUnitMongoDBDataAttribute/xUnitMongoDBDataAttribute/GenericProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitMongoDBDataAttribute
+{
+
+    /// <summary>
+    /// Unique Id interface
+    /// </summary>
+    public interface IUniqueId
+    {
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
+        Guid Id { get; set; }
+    }
+    /// <summary>
+    /// Generic Properties Class to store object content
+    /// </summary>
+    public class GenericProperties : IUniqueId
+    {
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity properties
+        /// </summary>
+        public IDictionary<string, KeyValuePair<object, bool>> entity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attrbutes applied to an entity
+        /// </summary>
+        public IList<IDictionary<string, KeyValuePair<object, bool>>> attributes { get; set; }
+
+    }
+}
---
+++ xUnitMongoDBDataAttribute/xUnitMongoDBDataAttribute/MongoDBRepository.cs
@@ -0,0 +1,78 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitMongoDBDataAttribute
+{
+    /// <summary>
+    /// MongoDB Repository Implementation
+    /// </summary>
+    public class MongoDBRepository
+    {
+        /// <summary>
+        /// Gets or sets the MongoDB Client
+        /// </summary>
+        public MongoClient client { get; set; }
+
+        /// <summary>
+        /// Gets or sets the MongoDB server
+        /// </summary>
+        public MongoServer server { get; set; }
+
+        /// <summary>
+        /// Gets or sets the MongoDB database
+        /// </summary>
+        public MongoDatabase database { get; set; }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MongoDBRepository"/> class.
+        /// </summary>
+        public MongoDBRepository()
+        {
+            client = new MongoClient(ConfigurationManager.AppSettings["connectionString"]);
+            server = client.GetServer();
+            database = server.GetDatabase(ConfigurationManager.AppSettings["databaseString"]);
+        }
+
+        /// <summary>
+        /// Get all Entities within the collection
+        /// </summary>
+        /// <typeparam name="T">Type of Entity</typeparam>
+        /// <param name="collectionName">Name of collection</param>
+        /// <returns></returns>
+        public IQueryable<T> GetAllEntities<T>(string collectionName)
+        {
+            var  collection = database.GetCollection<T>(collectionName);
+
+            return collection.FindAll().AsQueryable<T>();
+        }
+
+        /// <summary>
+        /// Create new Entity
+        /// </summary>
+        /// <typeparam name="T">Type of entity to create</typeparam>
+        /// <param name="collectionName">Collection name</param>
+        /// <param name="entity">Entity to create</param>
+        /// <returns>Created Entity</returns>
+        public T CreateEntity<T>(string collectionName, T entity) where T : class, IUniqueId
+        {
+            // If the Unique Id is not set create a new Guid
+            if (entity.Id == null)
+                entity.Id = new Guid();
+
+            var collection = database.GetCollection<T>(collectionName);
+            var result = collection.Insert(entity);
+
+            if (result.Ok)
+                return entity;
+            else
+            {
+                throw new Exception(result.ErrorMessage);
+            }
+        }
+    }
+}
